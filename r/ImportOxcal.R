72f88b96ae8daba7700ffcd7fbefcfa6de5c7f0d
richardjtelford <richard.telford@bio.uib.no>
2016-11-19 02:39:50 +0100

    chunk tags and plotting functions

--- r/ImportOxcal.R
+++ r/ImportOxcal.R
@@ -1,5 +1,5 @@
 #Import OxCal functions
-
+## ---- getVectors
 get.vectors <- function(js, target) {
   x <- js[grep(target, js)]
   lab <- gsub("^(ocd\\[\\d+\\]).*", "\\1", x )
@@ -9,6 +9,7 @@ get.vectors <- function(js, target) {
   setNames(res, lab)
 }
 
+## ---- getValues
 get.values <- function(js, target, name = "value", numeric = TRUE) {
   x <- js[grep(target, js)]
   val <- gsub(".+=(.+);", "\\1", x)
@@ -20,7 +21,7 @@ get.values <- function(js, target, name = "value", numeric = TRUE) {
   setNames(res, c("lab", name))
 }
 
-
+## ---- getOxcal
 get.OxCal <- function(js, posterior = TRUE){
   if(posterior){
     what <- "posterior"
@@ -42,6 +43,38 @@ get.OxCal <- function(js, posterior = TRUE){
     date <- r$start + 0:(length(prob) - 1) * r$resolution
     data_frame(lab = r$lab, depth = r$depth, date = date, prob = prob, comment  = r$comment, posterior = posterior)
   })
+  out$start <- NULL
+  out$resolution <- NULL
   out$date <- 1950 - out$date#convert to BP
   out
+}
+
+## ---- getAgemodel
+get.ageModel <- function(x, ci = c(0.023, 0.977)){
+  x %>% 
+    filter(posterior == TRUE) %>% 
+    group_by(depth, comment) %>% 
+    mutate(sumProb = cumsum(prob)) %>% 
+    mutate(sumProb = sumProb/max(sumProb)) %>% 
+    summarise(
+      medianDate = approx(x = sumProb, y = date, xout =  0.5)$y,
+      lowCI = approx(x = sumProb, y = date, xout = ci[1])$y,
+      highCI = approx(x = sumProb, y = date, xout = ci[2])$y
+    ) 
+}
+
+## ---- plotdatesmodel
+plot.dates_model <- function(dates, model, title){
+  dates %>% filter(grepl("Dep", comment)) %>% #just the dates
+    ggplot(aes(x = depth, y = date, size = prob, colour = posterior, group = comment)) +   
+    geom_ribbon(data = model, aes(x = depth, ymax = highCI, ymin = lowCI), inherit.aes = FALSE, alpha  = 0.2) +
+    geom_line(data = mod, aes(x = depth, y = medianDate), inherit.aes = FALSE, colour = "grey40") +
+    geom_line(alpha = 0.6) + 
+    scale_x_reverse() +
+    scale_size(range = c(0, 6)) +
+    coord_flip() +
+    labs(x = "Depth cm", y = "Date yr BP") + 
+    theme_bw() + 
+    guides(size = "none", colour = guide_legend(title = "Posterior", override.aes = list(size = 4))) + 
+    ggtitle(title)
 }
